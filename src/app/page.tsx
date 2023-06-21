4f80a0bd9d838a887a5793510bd12cb5010793a5
Robert Safin <robertsafin98@gmail.com>
2023-06-21 10:59:53 +0800

    re

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,6 @@ const HomePage = () => {
         <div className={styles.avatarAndContact}>
           <Image className={styles.avatar} src={`/me.png`} alt={'Robert Safin'} width={1000} height={1000} />
           <p className={styles.MDaboutText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla facilisi morbi tempus iaculis urna. Elementum sagittis vitae et leo duis. Interdum velit laoreet id donec ultrices tincidunt arcu. Blandit cursus risus at ultrices mi tempus imperdiet nulla. Lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum lectus. Faucibus in ornare quam viverra. Tempor commodo ullamcorper a lacus vestibulum sed. Facilisis leo vel fringilla est ullamcorper eget nulla facilisi. Urna porttitor rhoncus dolor purus non enim praesent elementum. Vehicula ipsum a arcu cursus vitae congue. Integer enim neque volutpat ac tincidunt vitae semper quis lectus. Enim facilisis gravida neque convallis a.
-            Eget egestas purus viverra accumsan in nisl nisi scelerisque. Eget sit amet tellus cras adipiscing enim eu turpis. Pretium lectus quam id leo in. Orci eu lobortis elementum nibh tellus. Sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque eu. Dictum sit amet justo donec enim diam vulputate. Urna neque viverra justo nec ultrices. Odio eu feugiat pretium nibh ipsum consequat nisl vel. Sit amet consectetur adipiscing elit ut aliquam purus sit. Dignissim convallis aenean et tortor at risus viverra adipiscing at.
           </p>
           <div className={styles.contacts}>
             <h1 className={styles.contactHeader}>Contact</h1>
@@ -51,6 +50,14 @@ const HomePage = () => {
             Amet mauris commodo quis imperdiet massa tincidunt nunc. Suspendisse potenti nullam ac tortor vitae purus. Rutrum quisque non tellus orci ac auctor augue mauris augue. Aliquet lectus proin nibh nisl condimentum id venenatis a. Eros donec ac odio tempor orci dapibus ultrices in. Id volutpat lacus laoreet non curabitur gravida arcu ac. Mi in nulla posuere sollicitudin aliquam. Proin sagittis nisl rhoncus mattis rhoncus. Dui vivamus arcu felis bibendum ut tristique et egestas quis. Eget mi proin sed libero enim sed. Id leo in vitae turpis. Suspendisse ultrices gravida dictum fusce ut placerat.
           </p>
         </div>
+
+
+
+
+
+
+
+      <div className={styles.cards}>
 
         <div className={styles.projectsContainer}>
           <h1 className={styles.projectHeader}>Projects</h1>
@@ -101,6 +108,8 @@ const HomePage = () => {
 
           </div>
         </div>
+      </div>
+
 
         <div className={styles.miscGroup}>
           <h1 className={styles.educationHeader}>Education</h1>
